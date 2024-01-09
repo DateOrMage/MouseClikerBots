b607e3f37166a36e2b7b5186c2f712c09be1256f
Aleksei Kudriavtsev <vatutu@gmail.com>
2024-01-09 15:47:25 +0300

    read excel data

---
+++ load_file.py
@@ -0,0 +1,49 @@
+from pandas import read_excel, DataFrame
+
+
+class LoadFile:
+    def __init__(self, input_folder: str) -> None:
+        self.input_path: str = input_folder
+        self.flag: bool = True
+        self.output_text: str = 'Файл успешно загружен.'
+
+    def load_excel(self):
+        df = read_excel(self.input_path)
+
+        return df
+
+    def check_columns(self, df: DataFrame) -> DataFrame:
+        try:
+            df = df[['ID', 'ACCOUNT_ID', 'CREATED', 'Кол-во координат',
+                     'Координаты с отпечатком времени в unix формате (кол-во миллисекунд с 01.01.1970)']]
+        except KeyError:
+            self.output_text = 'Error: Неверное название столбцов, см. руководство пользователя.'
+            self.flag = False
+
+        return df
+
+    @staticmethod
+    def check_nan(df: DataFrame) -> DataFrame:
+        if df.isnull().sum().sum() > 0:
+            df = df.dropna()
+
+        return df
+
+    def check_up(self, df: DataFrame) -> DataFrame:
+        df = self.check_columns(df)
+        if not self.flag:
+            return df
+        df = self.check_nan(df)
+
+        return df
+
+    def execute(self):
+        df = self.load_excel()
+        df = self.check_up(df)
+
+        return df, self.output_text, self.flag
+
+
+if __name__ == '__main__':
+    lf = LoadFile("C:\\Users\\Пользователь\\Downloads\\Координаты движения мыши Unix mini.xlsx")
+    data, text, flag = lf.execute()
